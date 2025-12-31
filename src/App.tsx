877115cb1e72e8e295912484942e74a64201df52
Vyshnavi Seethaka <vyshuseethaka@gmail.com>
2025-12-31 16:52:27 +0530

    Fix React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Browse from "./pages/Browse";
 import Solutions from "./pages/Solutions";
@@ -27,7 +28,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+
+      {/* 🔥 CRITICAL FIX */}
+      <BrowserRouter basename="/bookish-oasis">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/browse" element={<Browse />} />
